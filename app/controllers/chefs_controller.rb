2264992a3ed1a8b5b7b4243859a9360f7fed2c14
ari <ariprasath95@gmail.com>
2021-08-04 17:55:37 +0530

    chef pages

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -16,10 +16,25 @@ class ChefsController < ApplicationController
 		end
 
 		def show
+			@chef = Chef.find(params[:id])
 
 		end
 
 	end
+
+	def edit
+		@chef = Chef.find(params[:id])
+    end
+
+     def update
+         @chef = Chef.find(params[:id])
+         if @chef.update(chef_params)
+           flash[:success] = "Your account was updated successfully"
+           redirect_to @chef
+        else
+          render 'edit'
+        end  
+     end
 
 	private
 
